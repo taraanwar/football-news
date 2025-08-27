e8ca01d8dacdaf33732fc569a55a3bead23f7d82
taraanwar <taraanwar18@gmail.com>
2025-08-27 16:49:33 +0700

    update

--- football-news/settings.py
+++ football-news/settings.py
@@ -31,7 +31,7 @@ PRODUCTION = os.getenv('PRODUCTION', 'False').lower() == 'true'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["localhost", "127.0.0.1", "tara-nirmala-footballnewws.pbp.cs.ui.ac.id"]
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", "tara-nirmala-footballnews.pbp.cs.ui.ac.id"]
 
 
 # Application definition
